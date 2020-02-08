bf245826546a1c2387e082c6c72d6d7cb692e293
muhajirmuhajir <hajir.nf@gmail.com>
2020-02-08 19:13:25 +0700

    add likeRouter on index.js

--- router/index.js
+++ router/index.js
@@ -1,25 +1,27 @@
 const router = require('express').Router()
 const userRouter = require('./UserRouter')
+const likeRouter = require('./LikeRouter')
 
 router.get('/', (req, res) => {
     res.send('BCC In Glory')
 })
 
 router.use('/user', userRouter)
+router.use('/like', likeRouter)
 router.use(notFound)
 router.use(errorHandler)
 
-function notFound(req, res, next){
+function notFound(req, res, next) {
     res.status(404)
     const err = new Error("Page not found")
     next(err)
 }
 
-function errorHandler(err, req, res, next){
+function errorHandler(err, req, res, next) {
     res.status(res.statusCode || 500)
     const message = err.message || "Internal server error"
     res.json({
-        "message" : message
+        "message": message
     })
 }
 
