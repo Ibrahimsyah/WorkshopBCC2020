c5b185b049aef139dfacfe22352e14ce8e21b250
muhajirmuhajir <hajir.nf@gmail.com>
2020-02-08 20:14:10 +0700

    add dislike method

--- controller/LikeController.js
+++ controller/LikeController.js
@@ -2,7 +2,7 @@ const db = require('../database');
 
 exports.getAllLike = async (req, res, next) => {
     try {
-        const [rows] = await db.query('select * from like')
+        const [rows] = await db.query('select * from `like`')
         res.json({
             "success": true,
             "data": rows
