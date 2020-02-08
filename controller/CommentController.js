04efb746e92cfc7af87e86d3e2a7714e9987d7a1
muhajirmuhajir <hajir.nf@gmail.com>
2020-02-08 21:09:13 +0700

    fix getPostComment

--- controller/CommentController.js
+++ controller/CommentController.js
@@ -3,7 +3,7 @@ const db = require('../database');
 exports.getPostComment = async (req, res, next) => {
     try {
         const id = req.params.id
-        const comments = await db.query('select * from comment where id_post = ?', [id])
+        const [comments] = await db.query('select * from comment where id_post = ?', [id])
 
         res.json({
             success: true,
