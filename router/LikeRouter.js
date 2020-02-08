8b08f3740f6c699923c66a1014fef5e96ce9989e
muhajirmuhajir <hajir.nf@gmail.com>
2020-02-08 19:07:59 +0700

    add LikeRouter file

--- router/LikeRouter.js
+++ router/LikeRouter.js
@@ -1,6 +1,8 @@
-const router = require('express').router()
+const router = require('express').Router()
 const likeController = require('../controller/LikeController')
 
 router.get('/', likeController.getAllLike)
 router.post('/', likeController.likeAPost)
 router.delete('/', likeController.dislikeAPost)
+
+module.exports = router
