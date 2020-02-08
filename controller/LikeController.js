2ef5b495a122a64488f627aea61e6432366a0e47
muhajirmuhajir <hajir.nf@gmail.com>
2020-02-08 20:07:41 +0700

    add dislike method

--- controller/LikeController.js
+++ controller/LikeController.js
@@ -11,3 +11,36 @@ exports.getAllLike = async (req, res, next) => {
         next(err)
     }
 }
+
+
+exports.likeAPost = (req, res, next) => {
+    const userId = req.body.user_id
+    const postId = req.body.post_id
+    db.query('insert into `like`(id_post, id_user) values(?,?)', [postId, userId])
+        .then(() => {
+            res.json({
+                "success": true,
+                "message": "Post Liked"
+            })
+        })
+        .catch((err) => {
+            next(err)
+        })
+}
+
+
+exports.dislikeAPost = (req, res, next) => {
+    const userId = req.body.user_id
+    const postId = req.body.post_id
+
+    db.query('delete from `like` where id_post = ? and id_user = ?', [postId, userId])
+        .then(() => {
+            res.json({
+                "success": true,
+                "message": "Post dislike"
+            })
+        })
+        .catch((err) => {
+            next(err)
+        })
+}
