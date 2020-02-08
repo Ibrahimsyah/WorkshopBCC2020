a8c30a7e0820aa96fece289617aecfde2f68032e
muhajirmuhajir <hajir.nf@gmail.com>
2020-02-08 20:32:25 +0700

    add postComment method

--- controller/CommentController.js
+++ controller/CommentController.js
@@ -13,3 +13,22 @@ exports.getPostComment = async (req, res, next) => {
         next(err)
     }
 }
+
+
+exports.postComment = (req, res, next) => {
+    const user_id = req.params.user_id
+    const post_id = req.params.post_id
+    const content = req.params.content
+
+    db.query('insert into comment(id_post, id_user, content) values (?,?,?)', [post_id, user_id, content])
+        .then(() => {
+            res.json({
+                success: true,
+                message: "comment posted",
+                data: content
+            })
+        })
+        .catch((err) => {
+            next(err)
+        })
+}
